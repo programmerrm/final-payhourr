0d97820706e3cc35cd440883ae2798cd5b13eb67
programmerrm <programmerwebrm@gmail.com>
2025-08-01 18:17:55 +0600

    now file add

--- backend/wait-for-db.sh
+++ backend/wait-for-db.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "🚀 Starting Django migrations (SQLite)..."
+echo "🚀 Starting Django migrations..."
 
 python manage.py makemigrations
 python manage.py migrate
 
-echo "✅ Migrations done. Starting server..."
+echo "✅ Migrations done. Starting Gunicorn..."
 
-exec "$@"
+exec gunicorn your_project_name.wsgi:application --bind 0.0.0.0:8000 --workers 3
