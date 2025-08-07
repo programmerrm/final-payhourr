d81bc45a729a3a5a3aac54bca91243eadeb775c1
programmerrm <programmerwebrm@gmail.com>
2025-08-07 21:31:59 +0600

    final code

--- backend/app/settings/dev.py
+++ backend/app/settings/dev.py
@@ -26,12 +26,16 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3001',
     'http://127.0.0.1:8000',
+    'https://payhourr.com',
+    'https://api.payhourr.com',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3001',
     'http://127.0.0.1:8000',
+    'https://payhourr.com',
+    'https://api.payhourr.com',
 ]
 
 REST_FRAMEWORK['DEFAULT_SCHEMA_CLASS'] = 'drf_spectacular.openapi.AutoSchema'
